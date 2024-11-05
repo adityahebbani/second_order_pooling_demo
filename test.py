4864d1a0678e6def4848988b9e8eb7429502cbb3
Chris <totallynotherro@gmail.com>
2024-11-05 11:17:09 -0500

    added cifar100

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ from layers import SecondOrderPooling
 
 # Normalize and reshape test data
 x_test = x_test.astype('float32') / 255.0
-x_test = x_test.reshape(-1, 28, 28, 1)
+# x_test = x_test.reshape(-1, 28, 2 8, 1) # mnist dataset code
 
 # Load the model from the SavedModel format
 model = tf.keras.models.load_model('second_order_pooling_model', custom_objects={'SecondOrderPooling': SecondOrderPooling})
@@ -23,8 +23,10 @@ predictions = model.predict(x_test)
 # Show some sample predictions
 num_samples = 5
 for i in range(num_samples):
-    plt.imshow(x_test[i].reshape(28, 28), cmap='gray')
-    plt.title(f"True Label: {y_test[i]}, Predicted: {np.argmax(predictions[i])}")
+    #plt.imshow(x_test[i].reshape(28, 28), cmap='gray') # mnist code
+    plt.imshow(x_test[i]) # cifar code
+    # plt.title(f"True Label: {y_test[i]}, Predicted: {np.argmax(predictions[i])}") # mnist code
+    plt.title(f"True Label: {y_test[i][0]}, Predicted: {np.argmax(predictions[i])}") # cifar code
     plt.show()
 
 # Evaluate the model on the test set
--- train.py
+++ train.py
@@ -3,10 +3,12 @@ import tensorflow as tf
 from keras import layers, models, datasets
 import matplotlib.pyplot as plt
 from layers import SecondOrderPooling
+from keras.callbacks import EarlyStopping
 
 # Load the Fashion MNIST dataset
 # (x_train, y_train), (x_test, y_test) = datasets.fashion_mnist.load_data() # mnist dataset code
 (x_train, y_train), (x_test, y_test) = datasets.cifar10.load_data() # cifar dataset code
+# (x_train, y_train), (x_test, y_test) = datasets.cifar100.load_data() # cifar dataset code
 
 # Normalize and reshape the data
 x_train = x_train.astype('float32') / 255.0
@@ -25,7 +27,8 @@ x = layers.MaxPooling2D(pool_size=(2, 2))(x)
 # x = layers.GlobalAveragePooling2D()(x) # global average pooling
 x = layers.Flatten()(x) # no pooling control
 x = layers.Dense(128, activation='relu')(x)
-outputs = layers.Dense(10, activation='softmax')(x)
+# outputs = layers.Dense(10, activation='softmax')(x) # mnist or cifar 10
+outputs = layers.Dense(100, activation='softmax')(x) # cifar 100
 
 model = models.Model(inputs=inputs, outputs=outputs)
 
@@ -33,7 +36,11 @@ model = models.Model(inputs=inputs, outputs=outputs)
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
 # Train the model (TensorFlow will automatically use the GPU if available)
-model.fit(x_train, y_train, epochs=10, batch_size=32, validation_data=(x_test, y_test))
+# model.fit(x_train, y_train, epochs=10, batch_size=32, validation_data=(x_test, y_test))
+
+early_stopping = EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)
+
+model.fit(x_train, y_train, epochs=50, batch_size=32, validation_data=(x_test, y_test), callbacks=[early_stopping])
 
 # Save the trained model in SavedModel format
 model.save('second_order_pooling_model')  # Saves as a directory
